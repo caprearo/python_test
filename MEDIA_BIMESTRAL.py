e56119f60565bd6f30dd5e16c0e0b80834a3e950
Caroline Prearo <caroline.prearo@gmail.com>
2021-09-08 22:12:46 -0300

    Meu primeiro commit

---
+++ MEDIA_BIMESTRAL.py
@@ -0,0 +1,11 @@
+# Faça um Programa que peça as 4 notas bimestrais e mostre a média.
+
+nota1 = int(input('Digite a nota do primeiro bimestre: '))
+nota2 = int(input('Digite a nota do segundo bimestre: '))
+nota3 = int(input('Digite a nota do terceiro bimestre: '))
+nota4 = int(input('Digite a nota do quarto bimestre: '))
+
+print(f'A média do bimestre é: {(nota1+nota2+nota3+nota4)//4}.')
+
+# EXERCICIO CONCLUIDO COM SUCESSO
+
